68c195ca290a56cab797411977c64301d3770831
kevin <1129826190@qq.com>
2020-06-17 22:42:02 +0800

    day01

--- codes/07-ajax/day01/04-register/register.php
+++ codes/07-ajax/day01/04-register/register.php
@@ -1,2 +1,13 @@
 <?php
-echo 'hehe';
+// 1.获取前端传递用户名
+$name = $_GET['name'];
+// 2.获取数据库中所有用户名 select name from user;
+$names = ['zs','ls','wu'];
+// 3.判断
+// in_array(a, b); 判断数据a是否在数组b中
+//
+if (in_array($name, $names)) {
+    echo 'x';
+} else {
+    echo '√';
+}
