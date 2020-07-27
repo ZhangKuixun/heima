5f3500449f8b3fe20aab0b5753f2339318e55195
kevin <1129826190@qq.com>
2020-07-27 22:35:27 +0800

    金融项目

---
+++ codes/10-移动web/day05.金融项目实战1/04-wjs/js/index.js
@@ -0,0 +1,13 @@
+// 触屏轮播图
+touchBanner();
+
+// 实现触屏轮播图
+//  1.触屏开始，记录起始位置，停止定时器
+//  2.触屏移动，记录移动后位置，计算距离差值
+//  3.触屏结束，判断用户的滑动方向
+function touchBanner() {
+    var startX = 0;
+    var moveX = 0;
+    var distanceX = 0;
+
+}
