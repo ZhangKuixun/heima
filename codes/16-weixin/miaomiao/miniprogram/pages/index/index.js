2d9322f44feecd0791a9764471efe40e4087ea73
zhangkuixun <zhangkuixun@gmail.com>
2020-08-07 22:55:42 +0800

    喵喵

---
+++ codes/16-weixin/miaomiao/miniprogram/pages/index/index.js
@@ -0,0 +1,70 @@
+// miniprogram/pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls:[
+      "https://pic1.zhimg.com/80/v2-f61cdc49eddd69e9bd2b06587d3e094e_720w.jpg?source=1940ef5c",
+      "https://pic3.zhimg.com/80/v2-e1459355307e881617127f2d185cc3b2_720w.jpg?source=1940ef5c",
+      "https://pic1.zhimg.com/80/v2-4417a6b4921f30316f73af60b57596cb_720w.jpg?source=1940ef5c"
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
