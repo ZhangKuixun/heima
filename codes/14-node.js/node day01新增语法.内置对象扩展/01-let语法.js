be7c168f2088025a15960d7a9fb0790fa705bf34
kevin <1129826190@qq.com>
2020-09-14 22:44:26 +0800

    nodejs

---
+++ codes/14-node.js/node day01新增语法.内置对象扩展/01-let语法.js
@@ -0,0 +1,18 @@
+// let const 定义变量，关键字
+// var 定义变量
+
+// console.log(num);
+// var num = 100;
+// function num() {
+//     console.log('你好');
+// }
+
+
+// let定义变量
+// 特点：
+// 1.变量不会提示
+// 2.变量不能重复定义
+// 3.let const 定义变量都是块级作用域
+// console.log(num);
+// let num = 100;
+// console.log(num);
