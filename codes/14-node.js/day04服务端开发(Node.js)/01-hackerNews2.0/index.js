40b9ada6c9032645160b9bb75c5983e4caaa1cbf
kevin <1129826190@qq.com>
2020-09-27 21:10:24 +0800

    首页资源加载

--- codes/14-node.js/day04服务端开发(Node.js)/01-hackerNews2.0/index.js
+++ codes/14-node.js/day04服务端开发(Node.js)/01-hackerNews2.0/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-// const mime = require('mime');
+const mime = require('mime');
 const fs = require('fs');
 const path = require('path');
 const template = require('art-template');
@@ -8,7 +8,6 @@ const server = http.createServer();
 server.on('request', (req, res) => {
     //路由
     console.log(req.url, req.method);
-    // res.setHeader('conetnt-type', mime.getType(req.url));
     if (req.url.startsWith('/index') || req.url === '/') {
         // 读取数据，渲染数据
         console.log(path.join(__dirname, 'data', 'data.json'));
@@ -25,8 +24,18 @@ server.on('request', (req, res) => {
         res.end("detail")
     } else if (req.url.startsWith('/submit')) {
         res.end("submit")
+    } else if (req.url.startsWith('/assets')) { // 静态资源处理
+        // 读取文件，返回给浏览器即可
+        fs.readFile(path.join(__dirname, req.url), (err, data) => {
+            if (err) {
+                return console.log('读取失败', err);
+            }
+            res.setHeader('conetnt-type', mime.getType(req.url)); // 指定类型
+            res.end(data);
+        })
+
     } else {
-        res.end("non")
+        res.end('404 页面未找到')
     }
 })
 server.listen(9999, () => { console.log("http://localhost:9999 服务器已启动") });
