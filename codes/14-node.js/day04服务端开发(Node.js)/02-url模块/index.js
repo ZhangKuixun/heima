7907a92febbbcef75c1fddd9eb06fa1996f51c63
张奎勋 <zhangkuixun@gmail.com>
2020-09-28 09:10:26 +0800

    url模块

---
+++ codes/14-node.js/day04服务端开发(Node.js)/02-url模块/index.js
@@ -0,0 +1,11 @@
+// url.parse(url, 参数二); 解析url地址种各个数据
+// 参数二：是否解析queryString(url地址后面跟的参数)，默认false
+
+// url对象是用于解析url地址，能获取url地址的各个部分，但是内部不会解析查询字符串，
+// 当第二个参数设置true，是去调用queryString.parse()方法，将查询字符串转成对象
+
+const url = require('url');
+
+let str = "http://localhost:9999/details?id=0&name=zs&age=18";
+const obj = url.parse(str, true);
+console.log(obj.query.name, obj.query.age);
