a61079b5e0cffbe5deb242f998f8fd07fc85ace1
zhangkuixun <zhangkuixun@gmail.com>
2020-11-09 23:26:28 +0800

    监听器，过滤器

--- codes/15-vue/day03监听器.过滤器.生命周期/07-命名问题.js
+++ codes/15-vue/day03监听器.过滤器.生命周期/07-命名问题.js
@@ -4,3 +4,15 @@ let obj = {
     'mg-name': 'zs', // 不是数字 字母 _ \ $，用''包着
     [xxx]: '男' // 想读取xxx的值用[]
 }
+
+
+/* 
+
+'' "" ``(反引号)
+模板字符串``
+
+<div><p></p></div>
+*/
+let name = '春'
+let str2 = `<div><p>${name}</p></div>`
+console.log(str2);
