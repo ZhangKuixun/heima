ab72e1f47c37e1d79a8dbf473efc37857c7bbccc
zhangkuixun <zhangkuixun@gmail.com>
2020-11-15 14:45:16 +0800

    axios修改tudos案例

--- codes/15-vue/day04制造假数据接口.axios/todomvc-app-template/js/app.js
+++ codes/15-vue/day04制造假数据接口.axios/todomvc-app-template/js/app.js
@@ -10,18 +10,29 @@
             editId: -1, // 双击的item的位置
         },
         created() {
-            this.list = JSON.parse(localStorage.getItem("list")) || []
+            // this.list = JSON.parse(localStorage.getItem("list")) || []
+            axios.get("http://localhost:3000/list").then(res => {
+                this.list = res.data
+            })
         },
         methods: {
             // 添加任务
             addTodo(e) {
                 if (this.todoName != null && this.todoName.trim().length > 0) {
-                    const id = this.list.length > 0 ? this.list[this.list.length - 1].id + 1 : 1;
+                    // const id = this.list.length > 0 ? this.list[this.list.length - 1].id + 1 : 1;
                     // 添加到数据里面
-                    this.list.push({
-                        id,
+                    // this.list.push({
+                    //     id,
+                    //     name: this.todoName,
+                    //     done: false,
+                    // })
+                    axios.post("http://localhost:3000/list", {
                         name: this.todoName,
-                        done: false,
+                        done: false
+                    }).then(res => {
+                        if (res != null && res.data != null) {
+                            this.list.push(res.data)
+                        }
                     })
 
                     this.todoName = ''
@@ -30,14 +41,21 @@
             // 删除任务
             delTodo(id) {
                 // 过滤list.item.id != 当前id，并返回一个过滤后的数组
-                this.list = this.list.filter(v => v.id != id)
+                axios.delete(`http://localhost:3000/list/${id}`).then(res => {
+                    this.list = this.list.filter(v => v.id != id)
+                })
             },
             // 显示编辑
             showEdit(id) {
                 this.editId = id
             },
             // 隐藏编辑状态
-            hideEdit() {
+            hideEdit(e) {
+                axios.patch(`http://localhost:3000/list/${this.editId}`, {
+                    name: e.target.value
+                }).then(res => {
+                    console.log(res);
+                })
                 this.editId = -1
             },
             // 底部隐藏
@@ -49,6 +67,13 @@
             // 清除已完成，过滤出未完成的
             clearCompleted() {
                 this.list = this.list.filter(item => !item.done)
+            },
+            // 状态改变
+            stateChange(id) {
+                let todo = this.list.find(item => item.id == id) // 获取list的done
+                axios.patch(`http://localhost:3000/list/${id}`, {
+                    done: !todo.done
+                }).then(res => {})
             }
         },
         // 计算属性
@@ -70,14 +95,14 @@
             }
         },
         // 监听器
-        watch: {
-            list: {
-                deep: true,
-                handler(newVal) {
-                    console.log("改变了");
-                    localStorage.setItem("list", JSON.stringify(newVal));
-                }
-            }
-        }
+        // watch: {
+        //     list: {
+        //         deep: true,
+        //         handler(newVal) {
+        //             console.log("改变了");
+        //             localStorage.setItem("list", JSON.stringify(newVal));
+        //         }
+        //     }
+        // }
     })
 })(window);
