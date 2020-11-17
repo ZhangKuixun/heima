855579a88b6bfacc4c983a00f6002d8bdf0a5b08
zhangkuixun <zhangkuixun@gmail.com>
2020-11-17 21:48:09 +0800

    父传子

--- codes/15-vue/day05组件的通信.案例：开关灯/todomvc-app-template/js/app.js
+++ codes/15-vue/day05组件的通信.案例：开关灯/todomvc-app-template/js/app.js
@@ -4,7 +4,11 @@
     const vue = new Vue({
         el: '#app',
         data: {
-
+            list: [
+                { id: 1, name: '吃饭', completed: true },
+                { id: 2, name: '睡觉', completed: false },
+                { id: 3, name: '打豆豆', completed: false }
+            ]
         },
     })
 
--- codes/15-vue/day05组件的通信.案例：开关灯/todomvc-app-template/js/components/todo-list.js
+++ codes/15-vue/day05组件的通信.案例：开关灯/todomvc-app-template/js/components/todo-list.js
@@ -4,28 +4,17 @@ Vue.component('todo-list', {
 					<input id="toggle-all" class="toggle-all" type="checkbox">
 					<label for="toggle-all">Mark all as complete</label>
 						<ul class="todo-list">
-								<li class="completed">
+								<li :class="{completed:item.done}" v-for='item in list' :key>
 										<div class="view">
-												<input class="toggle" type="checkbox" checked>
-												<label>Taste JavaScript</label>
+												<input class="toggle" type="checkbox" v-model='item.done'>
+												<label>{{item.name}}</label>
 												<button class="destroy"></button>
 										</div>
 										<input class="edit" value="Create a TodoMVC template">
 								</li>
-								<li>
-										<div class="view">
-												<input class="toggle" type="checkbox">
-												<label>Buy a unicorn</label>
-												<button class="destroy"></button>
-										</div>
-										<input class="edit" value="Rule the web">
-								</li>
 						</ul>
 					</section>
 					`,
-    data() {
-        return {
-            msg: "ssss"
-        }
-    }
+    // 父传子，第二步，子组件通过props指定一下要接收的数据
+    props: ['list'],
 })
