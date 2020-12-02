2981af37364053377a39c47d9edefbf8f86cbaaf
zhangkuixun <zhangkuixun@gmail.com>
2020-12-02 14:49:57 +0800

    webpack

---
+++ codes/15-vue/day07脚手架项目/day07-webpack-01/src/index.js
@@ -0,0 +1,8 @@
+// js中引入jquery
+// import $ from 'jquery'
+
+// es6引入jquery,浏览器不能识别es6语法
+import $ from 'jquery'
+
+$('li:odd').css('background', 'red');
+$('li:even').css('background', 'green');
---
+++ codes/15-vue/day07脚手架项目/day07-webpack/dist/index.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("function component() {\r\n    var element = document.createElement('div');\r\n\r\n    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的\r\n    element.innerHTML = _.join(['Hello', 'webpack'], ' ');\r\n\r\n    return element;\r\n}\r\n\r\ndocument.body.appendChild(component());\n\n//# sourceURL=webpack://day07-webpack/./src/index.js?");
+/******/ })()
+;
