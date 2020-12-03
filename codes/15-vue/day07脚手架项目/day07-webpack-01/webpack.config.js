f1408ec9bfd2083676fa053452a7711b7d64b764
ZhangKuixun <1129826190@qq.com>
2020-12-03 09:48:43 +0800

    webpack

--- codes/15-vue/day07脚手架项目/day07-webpack-01/webpack.config.js
+++ codes/15-vue/day07脚手架项目/day07-webpack-01/webpack.config.js
@@ -1,9 +1,16 @@
+// 引入path模块
 const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
+    // 入口
+    entry: "src/index.js",
+    // 出口
     output: {
+        // 出口文件
         filename: 'index.js',
+        // 出口目录
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    // 模式
+    // module: 'development'
 };
