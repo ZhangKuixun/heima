61866108f509298f68e5ef695b71d924ad223a0d
ZhangKuixun <1129826190@qq.com>
2020-12-06 17:26:12 +0800

    no message

--- codes/15-vue/day07脚手架项目/day07-webpack-02/src/index.js
+++ codes/15-vue/day07脚手架项目/day07-webpack-02/src/index.js
@@ -5,5 +5,9 @@
 // 浏览器不能识别es6的语法import
 import $ from 'jquery'
 
+// 引入css
+import './assets/demo.css'
+import './assets/demo.less'
+
 $('li:odd').css('background', 'red');
 $('li:even').css('background', 'green');
--- codes/15-vue/day07脚手架项目/day07-webpack-02/webpack.config.js
+++ codes/15-vue/day07脚手架项目/day07-webpack-02/webpack.config.js
@@ -17,12 +17,6 @@ module.exports = {
         // 出口目录
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [{
-            test: /\.css$/i,
-            use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        }, ],
-    },
     // 模式
     mode: 'development',
     // 插件
@@ -37,5 +31,23 @@ module.exports = {
     devServer: {
         open: true,
         port: 3002
-    }
+    },
+    // 配置loader加载器
+    module: {
+        rules: [{
+                // 处理css
+                test: /\.css$/i,
+                // 写的顺序不能错，使用的顺序是从后往前
+                // 使用：
+                //   css-loader读取demo.css内容，放到一个模块里；
+                //   style-loader创建style标签，把模块的内容加载进style标签，插入到页面中
+                use: ['style-loader', 'css-loader'],
+            },
+            // 处理less
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            }
+        ],
+    },
 };
