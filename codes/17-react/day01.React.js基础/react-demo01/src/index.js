4e449091db3a15cce28394dc4c3fd0d29130a09d
ZhangKuixun <1129826190@qq.com>
2021-04-07 10:38:43 +0800

    no message

--- codes/17-react/day01.React.js基础/react-demo01/src/index.js
+++ codes/17-react/day01.React.js基础/react-demo01/src/index.js
@@ -1,21 +1,28 @@
 /**
- * 03-练习，创建这样的文件：
- * <ul id='app'>
- *     <li>春春1号</li>
- *     <li>春春2号</li>
- *     <li>春春3号</li>
- * </ul>
+ * 06-定时器
+ *
+ * 虚拟DOM
+ * react 数据发生变化的时候，是通过虚拟DOM更新
+ * angular - 脏检查（数据变了，就叫脏）
+ * react - 虚拟DOM
+ * vue - 细粒度更新 - 每个数据 = 对应一个watcher
+ *     - 中等粒度更新 - 每个组件 - 对应一个watcher - 每个组件内部使用虚拟DOM
+ *       (name改变了 => setter => Dep => watcher => 组件 => 虚拟DOM进行比较 => 把改变的地方更新掉)
  */
 
 // 1.引用包
 import React from 'react'
 import ReactDom from 'react-dom'
 
-// 2.创建react对象
-let li1 = React.createElement('li', null, '春春1号');
-let li2 = React.createElement('li', null, '春春2号');
-let li3 = React.createElement('li', null, '春春3号');
-let ul = React.createElement('ul', {id: 'app'}, li1, li2, li3);
+function tick() {
+    // 2.创建react对象
+    var div = <div>
+        <h3>时间更新</h3>
+        <p>{new Date().toLocaleString()}</p>
+    </div>
 
-// 3.渲染
-ReactDom.render(ul, document.getElementById('root'))
+    // 3.渲染
+    ReactDom.render(div, document.getElementById('root'))
+}
+
+let interval = setInterval(tick, 1000);
