a03db5766668c5dd7c964e2ba6a5908e91d07e03
ZhangKuixun <1129826190@qq.com>
2021-04-17 18:22:03 +0800

    评论案例

--- codes/17-react/day02React.js基础/react-demo02/src/index.js
+++ codes/17-react/day02React.js基础/react-demo02/src/index.js
@@ -1,42 +1,42 @@
 /**
- * 06-
+ * 12-评论案例
  *
  */
 
 // 1.引入核心包
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
 
+function Comment(props) {
+    return <li>
+        <p>评论人:{props.name}</p>
+        <p>评论内容:{props.content}</p>
+    </li>
+}
+
 // 2.类组件
-class Clock extends React.Component {
+class CommentList extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            date: new Date()
+            list: [
+                {id: 1, name: '春春', content: '今天好冷'},
+                {id: 2, name: '飞飞', content: '今天好热'},
+                {id: 3, name: '涛涛', content: '你们是484'}]
         }
     }
 
-    componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval)
-    }
-
-    // Clock组件会通过调用setState()来计划进行一次UI更新
-    tick() {
-        this.setState({
-            date: new Date()
-        })
-    }
-
     render() {
-        return <div>时间--{this.state.date.toLocaleTimeString()}</div>
+        return <div>
+            <h3>评论列表如下</h3>
+            <ul>
+                {this.state.list.map(item => {
+                    return <Comment key={item.id} name={item.name} content={item.content}/>
+                })}
+            </ul>
+        </div>
     }
 }
 
 // 3.渲染
-ReactDOM.render(<Clock/>, document.getElementById('root'))
-
+ReactDOM.render(<CommentList/>, document.getElementById('root'))
